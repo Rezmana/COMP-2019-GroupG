f5c9182ed71391e82085f4ea7c958deff8a98c7c
ZoinWhite <114575858+ZoinWhite@users.noreply.github.com>
2023-11-02 14:32:03 +0800

    Add files via upload
    update 2/11/2023, basic login and sign up page for user and admin respectively

---
+++ Admin-Signup-Redirect.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.href = "Admin-Login.html";
+}, 2000); // 2000毫秒（2秒）
---
+++ User-Signup-Redirect.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.href = "User-Login.html";
+}, 2000); // 2000毫秒（2秒）
