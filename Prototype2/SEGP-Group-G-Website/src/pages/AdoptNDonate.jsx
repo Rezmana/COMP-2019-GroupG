21e73b4afa761b167e7b554a00c0e25fd4ce8a3d
Rez_Colin <ironcolin12@gmail.com>
2024-03-10 17:33:12 +0800

    Created the map functionality

--- Prototype2/SEGP-Group-G-Website/src/pages/AdoptNDonate.jsx
+++ Prototype2/SEGP-Group-G-Website/src/pages/AdoptNDonate.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { MapContainer } from "react-leaflet"
+import "./AdoptNDonate.css"
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+
 
 export const AdoptNDonate = () => {
   return (
-    <div>AdoptNDonate</div>
-
+  <MapContainer center={[2.8821,101.2198]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+</MapContainer>
   )
 }
