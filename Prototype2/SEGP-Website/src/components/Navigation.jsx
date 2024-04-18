1918efe306e462fe74b263c654cc1628947d91a3
LaurieBeebee <lbeebee15@heathfieldcc.co.uk>
2024-04-19 01:20:49 +0800

    Formatting finished and Login complete
    Small error whereby the icon overlaps with the navbar when the page is shrunk. I cannot find a way around this right now but could be changed later

--- Prototype2/SEGP-Website/src/components/Navigation.jsx
+++ Prototype2/SEGP-Website/src/components/Navigation.jsx
@@ -1,15 +1,66 @@
 import React from "react"
 import "./Navbar.css";
 import {Link, NavLink} from "react-router-dom"
+import {useNavigate} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import { FaUserAlt } from "react-icons/fa";
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const navLogin = () => {
+        navigate('/Login');
+    }
+
+    const navSignup = () => {
+        navigate('/Signup');
+    }
+
+    const navLogout = () => {
+        sessionStorage.removeItem('username');
+        navigate('/Login');
+    }
+
+    const getButtons = () => {
+        if (sessionStorage.getItem('username')) {
+            return (
+                <div className="button-container">
+                    <div className="col">
+                        <FaUserAlt size={30}></FaUserAlt>
+                    </div>
+                    <div className="col">
+                        <div className="row">
+                            <button className="navButton" onClick={navLogout}>Logout</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="button-container">
+                    <div className="col">
+                        <FaUserAlt size={30}></FaUserAlt>
+                    </div>
+                    <div className="col">
+                        <div className="row">
+                            <button className="navButton" onClick={navLogin}>Login</button>
+                        </div>
+                        <div className="row">
+                            <button className="navButton" onClick={navSignup}>Signup</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+
     return (
+        <div className='Container'>
         <nav>
             <div className="Navigation">
                 <ul id="navbar">
-                    <li>
-                        <NavLink to ="/Login">Login/Signup</NavLink>
-                    </li>
                     <li>
                         <NavLink to ="/HomePage">Home</NavLink>
                     </li>
@@ -28,5 +79,7 @@ export const Navbar = () => {
                 </ul>
             </div>
         </nav>
+        {getButtons()}
+        </div>
     )
 }
