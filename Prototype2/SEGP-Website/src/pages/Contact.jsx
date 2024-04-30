4874894e1368e8c87a796d01521a6e1a0fe139c0
Guoxuan Xu <114575858+ZoinWhite@users.noreply.github.com>
2024-04-30 15:53:33 +0800

    Update Contact.jsx

--- Prototype2/SEGP-Website/src/pages/Contact.jsx
+++ Prototype2/SEGP-Website/src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import emailjs from '@emailjs/browser';
 import Email from '../components/Email';
 import "./Contact.css";
 import Rollingpic from "../components/Rollingpic";
+import Footer from "../components/Footer"
 
 
 export const Contact = () => {
@@ -33,20 +34,7 @@ export const Contact = () => {
 
       <Rollingpic></Rollingpic>
 
-    <footer className="footer">
-        <div className="contact-info-left">
-          <h2>Our Service Number</h2>
-          <p>Malaysia +60 12-3456-7890</p>
-          <p>China +86 123456790</p>
-          <p>UK +44 1234567890</p>
-        </div>
-        <div className="contact-info-right">
-          <h2>Our Email</h2>
-          <p>seaturtle@outlook.com</p>
-          <p>admin@outlook.com</p>
-          <p>service@outlook.com</p>
-        </div>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 };
