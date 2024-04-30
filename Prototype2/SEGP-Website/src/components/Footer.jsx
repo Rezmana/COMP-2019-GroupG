6defb4b8d2df08168ecd9613df2cebb01a7eb7c3
Guoxuan Xu <114575858+ZoinWhite@users.noreply.github.com>
2024-04-30 15:51:16 +0800

    Create Footer.jsx

---
+++ Prototype2/SEGP-Website/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+
+  return (
+    <div>
+    <footer className="footer">
+        <div className="contact-info-left">
+          <h2>Our Service Number</h2>
+          <p>Malaysia +60 12-3456-7890</p>
+          <p>China +86 123456790</p>
+          <p>UK +44 1234567890</p>
+        </div>
+        <div className="contact-info-right">
+          <h2>Our Email</h2>
+          <p>seaturtle@outlook.com</p>
+          <p>admin@outlook.com</p>
+          <p>service@outlook.com</p>
+        </div>
+      </footer></div>
+  )
+}
