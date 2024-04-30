bbe0c10d480146b7a3aa8879f85aa44355a85987
Guoxuan Xu <114575858+ZoinWhite@users.noreply.github.com>
2024-04-30 15:55:24 +0800

    Update TrackYourTurtle.jsx

--- Prototype2/SEGP-Website/src/pages/TrackYourTurtle.jsx
+++ Prototype2/SEGP-Website/src/pages/TrackYourTurtle.jsx
@@ -5,11 +5,13 @@ import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Tooltip, Polyline } from "react-leaflet"
 import { Icon, map, marker } from 'leaflet';
 import { Map } from '../components/Map';
+import Footer from "../components/Footer"
 
 export const TrackYourTurtle = () => {
     return(
         <div>
             <Map></Map>
+            <Footer></Footer>
         </div>
     )
 }
